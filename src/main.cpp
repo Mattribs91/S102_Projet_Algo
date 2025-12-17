0bb641ff5bc12252371b6d2a81c4c05d71793292
Matthéo Ribeiro <mattheoribeiro91@hotmail.com>
2025-12-17 21:10:49 +0100

    Commit because of a bug

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <vector>
 
-#include "Moteur.h"
 #include "Image.h"
+#include "Moteur.h"
 #include "Personnage.h"
 
 using namespace std;
 
-int main(int, char**) // Version speciale du main, ne pas modifier
+int main(int, char **) // Version speciale du main, ne pas modifier
 {
   // Initialisation du jeu
   Moteur moteur("Jeu vidéo relou à faire");
@@ -19,58 +19,56 @@ int main(int, char**) // Version speciale du main, ne pas modifier
 
   // ---CREATION DES IMAGES----
 
-  //Image coffre_ferme;
-  //Image coffre_ouvert;
+  // Image coffre_ferme;
+  // Image coffre_ouvert;
   Image personnage_simple, fond;
 
   try {
-    //coffre_ferme = Image(moteur,"assets/coffre_ferme.png");
-    //coffre_ouvert = Image(moteur,"assets/coffre_ouvert.png");
-    fond = Image(moteur,"assets/fond.png");
-    personnage_simple = Image(moteur,"assets/personnages.png");
+    // coffre_ferme = Image(moteur,"assets/coffre_ferme.png");
+    // coffre_ouvert = Image(moteur,"assets/coffre_ouvert.png");
+    fond = Image(moteur, "assets/fond.png");
+    personnage_simple = Image(moteur, "assets/personnages.png");
   } catch (runtime_error) {
     cerr << "Impossible de charger l'image" << endl;
   }
 
-  Personnage personnage(0, 0, personnage_simple, BAS, 0, 0); //Creation de l'objet personnage
-  Personnage ennemi1(5 * TAILLE_CASE, 0, personnage_simple, BAS, 2, 1);
-  Personnage ennemi2(0, 5 * TAILLE_CASE, personnage_simple, BAS, 3, 0);
+  Avatar personnage(0, 0, personnage_simple, BAS, 0,
+                    0); // Creation de l'objet personnage
+  Ennemi ennemi1(5, 0, personnage_simple, BAS, 2, 1);
+  Ennemi ennemi2(0, 5, personnage_simple, DROITE, 3, 0);
 
   // Boucle de jeu, appelee a chaque fois que l'ecran doit etre mis a jour
   // (en general, 60 fois par seconde)
-  while (!quitter)
-  {
+  while (!quitter) {
     // I. Gestion des evenements
     Evenement evenement = moteur.evenementRecu();
-    while (evenement != AUCUN)
-    {
-      switch (evenement)
-      {
-        // QUITTER = croix de la fenetre ou Echap
-        case QUITTER_APPUYE:
-          quitter = true;
-          break;
-        // TODO: gerer les autres evenements
-        /*case ESPACE_APPUYE:
-          ouvert = true;
-          break;
-        case ESPACE_RELACHE:
-          ouvert = false;
-          break;*/
-        case HAUT_APPUYE:
-          personnage.regarderHaut();
-          break;
-        case DROITE_APPUYE:
-          personnage.regarderDroite();
-          break;
-        case BAS_APPUYE:
-          personnage.regarderBas();
-          break;
-        case GAUCHE_APPUYE:
-          personnage.regarderGauche();
-          break;
-        default:
-          break;
+    while (evenement != AUCUN) {
+      switch (evenement) {
+      // QUITTER = croix de la fenetre ou Echap
+      case QUITTER_APPUYE:
+        quitter = true;
+        break;
+      // TODO: gerer les autres evenements
+      /*case ESPACE_APPUYE:
+        ouvert = true;
+        break;
+      case ESPACE_RELACHE:
+        ouvert = false;
+        break;*/
+      case HAUT_APPUYE:
+        personnage.allerHaut();
+        break;
+      case DROITE_APPUYE:
+        personnage.allerDroite();
+        break;
+      case BAS_APPUYE:
+        personnage.allerBas();
+        break;
+      case GAUCHE_APPUYE:
+        personnage.allerGauche();
+        break;
+      default:
+        break;
       }
 
       evenement = moteur.evenementRecu();
@@ -80,17 +78,20 @@ int main(int, char**) // Version speciale du main, ne pas modifier
     // TODO: faire bouger vos personnages, etc.
 
     // III. Generation de l'image à afficher
-    moteur.initialiserRendu(); // efface ce qui avait ete affiche precedemment et reinitalise en ecran noir
+    moteur.initialiserRendu(); // efface ce qui avait ete affiche precedemment
+                               // et reinitalise en ecran noir
 
-    fond.dessiner(0, 0); //Affiche l'image de fond
-    personnage.dessiner(); //On dessine l'objet personnage
+    fond.dessiner(0, 0);   // Affiche l'image de fond
+    personnage.dessiner(); // On dessine l'objet personnage
+    ennemi1.avancer();
+    ennemi2.avancer();
     ennemi1.dessiner();
     ennemi2.dessiner();
 
     /*if (ouvert) {
-      coffre_ouvert.dessiner(0,position_y + 0.2); //Coffre ouvert si espace appuye
-    } else {
-      coffre_ferme.dessiner(0,position_y + 0.2); //Coffre ferme si espace relache
+      coffre_ouvert.dessiner(0,position_y + 0.2); //Coffre ouvert si espace
+    appuye } else { coffre_ferme.dessiner(0,position_y + 0.2); //Coffre ferme si
+    espace relache
     }*/
 
     // TODO: afficher vos personnages, objets, etc.
